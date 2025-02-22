59b71189b910279b940cb4c2d7abd27ed0150e70
Vardhman Jain <vardhmanjain2003@gmail.com>
2025-02-22 16:07:59 +0530

    [Vardhman Jain] Adding UC3_HelloRest

--- restApiSecond/src/main/java/com/springSecond/restApiSecond/RestController/RestApi.java
+++ restApiSecond/src/main/java/com/springSecond/restApiSecond/RestController/RestApi.java
@@ -2,6 +2,7 @@ package com.springSecond.restApiSecond.RestController;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +16,11 @@ public class RestApi {
     public String message(Model model) {
         model.addAttribute("message", "Hello World!");
         return "message";
+    }
+
+    @RequestMapping("/uc3")
+    public String index(){
+        return "hello from vardhman";
     }
 
 }
