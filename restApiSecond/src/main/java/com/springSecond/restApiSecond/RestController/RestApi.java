4b2aaf4515b9c3bcbd3b99dc708b5a3ae20c3ff2
Vardhman Jain <vardhmanjain2003@gmail.com>
2025-02-22 16:00:51 +0530

    [Vardhman Jain] Adding UC2_HelloController

--- restApiSecond/src/main/java/com/springSecond/restApiSecond/RestController/RestApi.java
+++ restApiSecond/src/main/java/com/springSecond/restApiSecond/RestController/RestApi.java
@@ -1,4 +1,6 @@
 package com.springSecond.restApiSecond.RestController;
+
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +10,11 @@ public class RestApi {
     String fn(){
         return "Hello from BridgeLabz";
     }
+
+    @GetMapping("/message")
+    public String message(Model model) {
+        model.addAttribute("message", "Hello World!");
+        return "message";
+    }
+
 }
